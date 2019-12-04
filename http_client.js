67a114c7aa85fa03a7d4ef3a428e3f65f665f8b4
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2019-12-04 10:57:27 -0800

    some work with while/break/continue

---
+++ http_client.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.get(process.argv[2], function(response) {
+	response.setEncoding('utf8');
+	response.on('error', function(err) {
+		console.log(err);
+	});
+	response.on('data', function(chunk) {
+		console.log(chunk);
+	});
+	// response.on('end', function(end) {
+	// 	console.log(end);
+	// });
+
+	
+});
